8da03cd2fc6bcb9d2d9acd222550d123a7f116a0
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-06-13 06:01:25 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    // Side nav 
+    $('.sidenav').sidenav();
+
+    const header = $('header');
+    const range = 200;
+    // Adds opacity on scroll
+    $(window).on('scroll', function () {
+    const scrollTop = $(this).scrollTop(),
+        height = header.outerHeight(),
+        offset = height / 1.1,
+        calc = 1 - (scrollTop - offset + range) / range;
+
+        header.css({ 'opacity': calc });
+
+        if (calc > '1') {
+            header.css({ 'opacity': 1 });
+        } else if ( calc < '0' ) {
+            header.css({ 'opacity': 0 });
+        }
+
+    });
+
+});
