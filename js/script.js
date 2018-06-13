77375dbf45d1842d09f65dda24cddb5f4da4abd5
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-06-13 16:44:55 +0530

    set smooth scrolling on website content navigation

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
     // Side nav 
     $('.sidenav').sidenav();
 
+    //Allows smooth scrolling of the content to certain element on click
+    $('.scrollspy').scrollSpy();
+
+    //set navbar text color to black by default
+    $('nav .brand-logo, nav ul a, nav a').css({color:'black'});
+
     const header = $('header');
     const range = 200;
     // Adds opacity on scroll
